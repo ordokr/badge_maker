5bdf1cc528c93fdf565d0ca758de1a597e3067a9
ordokr <tvail@wgu.edu>
2024-10-01 15:28:49 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
-  get "/examples" => "examples#index"
-  # define routes here
+BadgeMaker::Engine.routes.draw do
+  get "/badges" => "badges#index", as: :badges
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw do
+  mount ::BadgeMaker::Engine, at: "/badge-maker"
+end
