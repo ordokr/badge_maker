add99926839c4dea5a46bcf652b8e5059354f420
ordokr <tvail@wgu.edu>
2024-10-01 17:19:50 -0400

    Update badge.rb

--- lib/badge_maker/models/badge.rb
+++ lib/badge_maker/models/badge.rb
@@ -2,7 +2,7 @@
 
 module BadgeMaker
   class Badge < ActiveRecord::Base
-    self.table_name = "badges"
+    self.table_name = "badge_maker_badges"  # Or "badges" if using core Discourse table
 
     validates :name, presence: true
     validates :description, presence: true
