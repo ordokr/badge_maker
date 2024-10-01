feed7aa2c523fca205c06fe09043e80ab66c2907
ordokr <tvail@wgu.edu>
2024-10-01 16:53:56 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 BadgeMaker::Engine.routes.draw do
   resources :badges, only: [:index, :create, :show]
 end
 
 Discourse::Application.routes.draw do
-  mount ::BadgeMaker::Engine, at: '/badge_maker', as: 'custom_badge_maker'
+  mount ::BadgeMaker::Engine, at: '/badge_maker', as: 'badge_maker_plugin'  # Changed from 'custom_badge_maker'
 end
