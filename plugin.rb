862de0f41e575e1dae40d7ce594e1538ac5d0ff1
ordokr <tvail@wgu.edu>
2024-10-01 15:41:19 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
-# name: badge-maker
-# about: A plugin to award badges based on user achievements
+# name: badge maker
+# about: A plugin to manage badges in Discourse.
 # version: 0.1.0
 # authors: Your Name
-# url: http://yourpluginurl.com
-# required_version: 2.7.0
+# url: https://github.com/yourusername/badge_maker
 
 enabled_site_setting :badge_maker_enabled
+
+module BadgeMaker
+  class Engine < ::Rails::Engine
+    engine_name "badge_maker"
+    isolate_namespace BadgeMaker
+  end
+end
 
 require_relative "lib/badge_maker/engine"
 
 after_initialize do
-  # Any code to run after Discourse has finished booting
+  # Code which should run after Rails has finished booting
 end
